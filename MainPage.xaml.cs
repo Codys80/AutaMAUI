d09e6dd26a8d663334084fbb4e04285d96bec509
CodyS80 <bartolosemolo@gmail.com>
2025-11-05 09:35:52 +0100

    Update 2
    Ukończenie logiki

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,64 +4,91 @@ namespace AutaMAUI
 {
     public partial class MainPage : ContentPage
     {
-        static int CENA_BAZOWA = 75000;
-        int CENA_LAKIER = 9000;
-        int CENA_FELGI = 7000;
-        int CENA_CZUJNIKI = 6500;
-        int CENA_CLIMATRONIC = 8500;
-        int CENA_NAWIGACJA = 5000;
+        Auto auto1 = new Auto();
+        string msg = "";
         class Auto
         {
+            public double CENA_LAKIER { get; set; }
+            public double CENA_FELGI { get; set; }
+            public double CENA_CZUJNIKI { get; set; }
+            public double CENA_CLIMATRONIC { get; set; }
+            public double CENA_NAWIGACJA { get; set; }   
             public string Color { get; set; }
             public string Felgs { get; set; }
             public bool[] Extras { get; set; }
             public double BasePrice { get; set; }
             public Auto(string Color, string Felgs, bool[] Extras)
             {
-                Extras = new bool[2];
-                for (int i = 0; i < 3; i++)
-                {
-                    this.Extras[i] = Extras[i];
-                }
-                this.Color = Color;
+                this.Extras = [false, false, false];
+                this.Color = Color;  
                 this.Felgs = Felgs;
                 this.BasePrice = 75000;
-            }
+                this.CENA_LAKIER = 0;
+                this.CENA_FELGI = 0;
+                this.CENA_CZUJNIKI = 6500;
+                this.CENA_CLIMATRONIC = 8500;
+                this.CENA_NAWIGACJA = 5000;
 
+            }
 
             public Auto()
             {
-                Extras = new bool[2];
-                for (int i = 0; i < 3; i++)
-                {
-                    this.Extras[i] = false;
-                }
+                this.Extras = [false, false, false];
                 this.Color = "Szary";
                 this.Felgs = "Stalowe";
-                this.BasePrice = Convert.ToDouble(CENA_BAZOWA);
+                this.BasePrice = 75000;
+                this.BasePrice = 75000;
+                this.CENA_LAKIER = 0;
+                this.CENA_FELGI = 0;
+                this.CENA_CZUJNIKI = 6500;
+                this.CENA_CLIMATRONIC = 8500;
+                this.CENA_NAWIGACJA = 5000;
 
             }
         }
         void handleChange(object sender, EventArgs e)
         {
-            Auto auto1 = new Auto();
-            auto1.Color = pckColor.SelectedItem.ToString();
+            imgCar.Source = pckColor.Items[pckColor.SelectedIndex].ToLower() + ".png";
+            if (pckColor.SelectedIndex == 3) { imgCar.Source = "zolty.png"; }
+            auto1.Color = pckColor.Items[pckColor.SelectedIndex];
             if (sender is RadioButton) { auto1.Felgs = ((RadioButton)sender).Content.ToString(); }
             auto1.Extras[0] = chkParking.IsChecked;
             auto1.Extras[1] = chkClima.IsChecked;
             auto1.Extras[2] = chkNav.IsChecked;
-
-            lblBase.Text = "Cena bazowa: " + auto1.Color;
-            lblColor.Text = "Lakier: " + auto1.Color;
-            lblFelgs.Text = "Felgi: " + auto1.Felgs;
-            lblClima.Text = "Climatronic: ";
-            lblOverall.Text = "Razem: ";
+            msg = "";
+            msg += "Cena bazowa: " + auto1.BasePrice + " PLN\n";
+            if (auto1.Color == "Szary") { auto1.CENA_LAKIER = 0; }
+            else { auto1.CENA_LAKIER = 9000; msg += "Lakier: " + auto1.Color + " " + auto1.CENA_LAKIER + " PLN\n";  }
+            if (auto1.Felgs == "Aluminiowe") { auto1.CENA_FELGI = 7000; msg += "Felgi: " + auto1.Felgs + " " + auto1.CENA_FELGI + " PLN\n"; }
+            else auto1.CENA_FELGI = 0;
+            if (auto1.Extras[0]) { 
+                auto1.CENA_CZUJNIKI = 6500; 
+                msg += "Czujniki parkowania: " + auto1.CENA_CZUJNIKI + " PLN\n";
+            } 
+            else {
+                auto1.CENA_CZUJNIKI = 0;
+            }
+            if (auto1.Extras[1]) {
+                auto1.CENA_CLIMATRONIC = 8500;
+                msg += "Climatronic: " + auto1.CENA_CLIMATRONIC + " PLN\n";
+            } 
+            else {
+                auto1.CENA_CLIMATRONIC = 0;
+            }
+            if (auto1.Extras[2]) {
+                auto1.CENA_NAWIGACJA = 5000; 
+                msg += "Nawigacja: " + auto1.CENA_NAWIGACJA + " PLN\n";
+            } 
+            else {
+                auto1.CENA_NAWIGACJA = 0;
+            }
+            double temp = auto1.BasePrice + auto1.CENA_LAKIER + auto1.CENA_FELGI + auto1.CENA_CZUJNIKI + auto1.CENA_CLIMATRONIC + auto1.CENA_NAWIGACJA;
+            msg += "\nRazem: " + temp.ToString() + " PLN";
+            lblDisplay.Text = msg;
         }
         public MainPage()
         {
-            //new Auto(pckColor.SelectedItem.ToString(), FelgiGroup.SelectedItem.ToString(), 100000, new bool[] { chkClima.IsChecked });
             InitializeComponent();
-            Auto auto1 = new Auto();
             pckColor.SelectedIndex = 0;
         }
 
